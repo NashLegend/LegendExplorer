4e682efa37fd150f1833585df90dca9bbb8e2264
NashLegend <panzhihuipzh@gmail.com>
2014-08-07 14:19:33 +0800

    Empty Activities Added//TODO

---
+++ src/com/example/legendexplorer/ImageBrowseActivity.java
@@ -0,0 +1,15 @@
+package com.example.legendexplorer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ImageBrowseActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_image_browse);
+	}
+}
